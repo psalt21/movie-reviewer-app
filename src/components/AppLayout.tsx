a71526431bf55664293e6301bacf7bbcc5ee12d0
Phil Salter <philipleesalter@gmail.com>
2023-03-20 15:55:38 -0600

    added navbar and moved signout link inside and removed from inside page content

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./Navbar";
+
 export default function AppLayout({children}: {children: React.ReactNode}) {
     return (
+        <>
+        <Navbar />
         <div className='flex h-screen bg-white'>
             {children}
         </div>
+        </>
     )
 }
